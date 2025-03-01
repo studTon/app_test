2c846f62b2968685a031eba986f8322f4683daed
studTon <evertonsdasilva1@gmail.com>
2025-02-28 22:05:47 -0300

    Change app UI Following codelabs tutorial until TextTheme change; Build with some creativity.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'App Test',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green, secondary: const Color.fromARGB(255, 170, 146, 8)),
         ),
         home: MyHomePage(),
       ),
@@ -27,19 +27,58 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea built following the tutorial from Google code labs:'),
-          Text(appState.current.asLowerCase),
+          Text('An AMAZING idea built following the tutorial from Google code labs:'),
+          BigCard(pair: pair),
+
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Change!'),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onSecondary
+    );
+
+    return Card(
+      color: theme.colorScheme.secondary,
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Text(pair.asSnakeCase, style: style,),
       ),
     );
   }
